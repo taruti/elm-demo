2113d9e78cf82d329ca8c770ed41cd9316a4ea9f
Taru Karttunen <taruti@taruti.net>
2016-02-08 17:15:16 +0200

    Real example code

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,69 @@
 module Main where
 
-import Html exposing (text)
+import Color exposing(..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Lazy exposing (lazy, lazy2, lazy3)
+import Json.Decode as Json
+import Mouse
+import Signal exposing (Signal, Address)
+import String
+import Window
 
+main : Signal Element
 main =
-    text "Hello, world."
+  Signal.map2 view Window.dimensions (Signal.foldp update model0 input)
+
+input : Signal Action
+input = Signal.sampleOn Mouse.isDown (Signal.map m2act Mouse.position)
+
+m2act : (Int,Int) -> Action
+m2act (x,y) =   Add {x=x,y=y}
+
+-- MODEL
+
+type alias Coord = {x:Int, y:Int}
+type Model = Model (List Coord)
+model0 = Model []
+
+-- UPDATE
+
+type Action = Reset | Add Coord
+
+update : Action -> Model -> Model
+update action (Model xs) =
+  case action of
+    Reset -> Model []
+    Add x -> Model (x::xs)
+
+-- VIEW
+
+view : (Int,Int) -> Model -> Element
+view (w,h) (Model xs) =
+  let h' = h-100
+  in flow down [
+  collage w (h') (List.map (draw (w,h')) xs),
+  headShow xs, headRel (w,h') xs]
+
+headShow xs = case xs of
+  [] -> show ""
+  (x::_) -> show x
+headRel wh xs = case xs of
+  [] -> show ""
+  ({x,y}::_) -> show (relative wh (x,y))
+
+
+draw : (Int,Int) -> Coord -> Form
+draw wh {x,y} =
+  let
+  (rx,ry) = relative wh (x,y)
+  in circle 20 |> filled clearGrey |> move (toFloat rx, toFloat ry)
+
+relative (w,h) (x,y) = (x - w//2, h//2 - y)
+
+clearGrey : Color
+clearGrey =
+  rgba 111 111 111 0.6
